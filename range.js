9b0ab053777ea6da4fe67f44ab0992704943520a
Nikita Lavrentev <n.lawrentiew2017@yandex.ru>
2022-08-20 17:14:27 +0300

    rangeRight

--- range.js
+++ range.js
@@ -1,22 +1,27 @@
-const baseRange = (start, end, step) => {
+const baseRange = (start, end, step, isRight) => {
     let index = -1;
     let length = Math.max(Math.ceil((end - start) / (step || 1)), 0);
     const result = new Array(length);
 
     while (length--) {
-        result[++index] = start;
+        result[isRight ? length : ++index] = start
         start += step;
     }
 
     return result;
 }
 
-export  default function range(start = 0, end, step) {
+export function range(start = 0, end, step, isRight) {
     if (end === undefined) {
         end = start;
         start = 0;
     }
 
     step = step === undefined ? (start < end ? 1 : -1) : step;
-    return baseRange(start, end, step);
+    return baseRange(start, end, step, isRight);
 }
+
+export function rangeRight(start, end, step) {
+    return range(start, end, step, true);
+}
+
